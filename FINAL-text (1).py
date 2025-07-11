1d4789cb8eae1e1fe097463d02a74a8d245bc25c
etaverasx <etaverasx@gmail.com>
2025-07-11 14:59:16 -0400

    Describe what you changed

--- FINAL-text (1).py
+++ FINAL-text (1).py
@@ -1,7 +1,7 @@
-
+#Made a change -- checking 
 # Step 1: Import necessary libraries
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from IPython.display import display
